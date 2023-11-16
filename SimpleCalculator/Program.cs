1fe18b42a4f8b0de6aa620e3ab2ac07eeab7a8a0
fatchulf17 <96380224+fatchulf17@users.noreply.github.com>
2023-11-17 02:14:00 +0700

    all

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0.0)
+            {
+                throw new DivideByZeroException("ERROR: Zero Division Error");
+            }
+
+            return num1 / num2;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator ( + - * / ): ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Calculator calculator = new Calculator();
+
+            try
+            {
+                double answer = PerformCalculation(calculator, num1, op, num2);
+                Console.WriteLine($"Answer: {answer}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static double PerformCalculation(Calculator calculator, double num1, string op, double num2)
+        {
+            switch (op)
+            {
+                case "+":
+                    return calculator.Add(num1, num2);
+
+                case "-":
+                    return calculator.Subtract(num1, num2);
+
+                case "*":
+                    return calculator.Multiply(num1, num2);
+
+                case "/":
+                    return calculator.Divide(num1, num2);
+
+                default:
+                    throw new InvalidOperationException("ERROR: Invalid Operator");
+            }
+        }
+    }
+}
---
+++ SimpleCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using Assert = NUnit.Framework.Assert;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Subtract_TwoNumbers_ReturnsDifference()
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Subtract(5, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Multiply_TwoNumbers_ReturnsProduct()
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Multiply(2, 3);
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Divide_TwoNumbers_ReturnsQuotient()
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Divide(6, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Divide_ByZero_ThrowsDivideByZeroException()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(6, 0));
+        }
+    }
+}
